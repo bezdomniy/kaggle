e5ca051a04d1bac281b53525109b18cf8eedf046
Ilia Chibaev <ilia.shibaev@gmail.com>
2017-12-28 18:01:39 +1100

    working at 88.6% acc

--- voice/model_cnn.py
+++ voice/model_cnn.py
@@ -2,6 +2,7 @@ import numpy as np
 import datetime
 import os
 import tensorflow as tf
+from sklearn.utils import shuffle
 from voice import load_data,max_len
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
@@ -9,19 +10,52 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 checkpoints_dir = "./checkpoints"
 
 batch_size = 200
-iterations = 100000
-#max_len = 86
+iterations = 20001
+train_size = 50121
 
 def getTrainBatch(spectrograms,labels,lengths):
-    sample= np.random.randint(spectrograms.shape[0], size=batch_size)
+    #sample= np.random.randint(spectrograms.shape[0], size=batch_size)
+    sample= np.random.randint(train_size, size=batch_size)
 
     arr = spectrograms[sample]
     lengths = lengths[sample]
     lab = labels[sample]
     return arr, lengths, lab
 
-def run():
-    spectrograms,cats,lengths = load_data()
+def getData(spectrograms,labels,lengths, test=False):
+    if test:
+        return spectrograms[train_size:], lengths[train_size:], labels[train_size:]
+    else:
+        return spectrograms[:train_size], lengths[:train_size], labels[:train_size]
+            
+
+def test(val_data, val_lengths, val_labels):
+    saver = tf.train.import_meta_graph('./checkpoints/trained_model.ckpt-20000.meta')
+
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        saver.restore(sess, './checkpoints/trained_model.ckpt-20000')
+
+        graph = tf.get_default_graph()
+
+        input_data = graph.get_tensor_by_name("inputs:0")
+        labels = graph.get_tensor_by_name("labels:0")
+        input_lengths = graph.get_tensor_by_name("input_lengths:0")
+
+        accuracy = graph.get_tensor_by_name("accuracy:0")
+        print("Model restored.")
+
+        accuracies = []
+
+        for i in range(0,val_data.shape[0],batch_size):  
+            accuracy_value = sess.run([accuracy],{input_data: val_data[i:i+batch_size], labels: val_labels[i:i+batch_size]})
+            print("Acc: ",accuracy_value)
+            accuracies.append(accuracy_value)
+        print("Test Accuracy = {:.3f}".format(np.mean(accuracies)))
+
+
+def train(spectrograms,lengths, cats):
+    #spectrograms,cats,lengths = load_data()
 
     input_data, labels, input_lengths, dropout_keep_prob, optimizer, accuracy, loss = \
     define_graph()
@@ -47,8 +81,6 @@ def run():
     for i in range(iterations+1):
         batch_data, batch_lengths, batch_labels = getTrainBatch(spectrograms,cats,lengths)
 
-        #val_data, val_labels = getValBatch()
-
         sess.run(optimizer, {input_data: batch_data, input_lengths: batch_lengths, labels: batch_labels, dropout_keep_prob: 0.5})
         if (i % 50 == 0):
             accuracy_value, train_summ = sess.run(
@@ -72,10 +104,8 @@ def run():
     sess.close()
 
 def define_graph():
-    bidirectional = False
-    fully_connected_units = 0    # 0 for no fully connected layer
-    num_layers = 2    # only works with non-bidirectional LSTM
-
+    fully_connected_units = 128    
+ 
     # Length vector for 0 padded tensor
     def length(data):
         length = tf.reduce_sum(tf.sign(data), 1)
@@ -85,6 +115,8 @@ def define_graph():
     inputs = tf.placeholder(tf.float32, [batch_size, max_len, 129], name="inputs")
     labels = tf.placeholder(tf.int8, [batch_size, 30], name="labels")
     input_lengths = tf.placeholder(tf.int32, [batch_size], name="input_lengths")
+
+
 
     dropout_keep_prob = tf.placeholder_with_default(1.0, shape=())
 
@@ -101,10 +133,7 @@ def define_graph():
     
     output = tf.reshape(output, [batch_size, 7 * 8 * 128])
 
-    fully_connected = tf.contrib.layers.fully_connected(
-        output, 128, activation_fn=tf.sigmoid)
-        #fully_connected = tf.contrib.layers.dropout(
-        #    fully_connected, dropout_keep_prob)
+    fully_connected = tf.layers.dense(output, fully_connected_units, activation_fn=tf.sigmoid)
 
     dropout = tf.layers.dropout(inputs=fully_connected, rate=0.5)
 
@@ -126,5 +155,17 @@ def define_graph():
     accuracy = tf.reduce_mean(tf.cast(correct_preds, tf.float32),name="accuracy")
 
     return inputs, labels,input_lengths, dropout_keep_prob, optimizer, accuracy, loss
+
+
+
+def run():
+    spectrograms,cats,lengths = load_data()
+    spectrograms,cats,lengths = shuffle(spectrograms,cats,lengths, random_state=0)
+
+    train_data, train_lengths, train_labels = getData(spectrograms,cats,lengths)
+    train(train_data, train_lengths, train_labels)
+
+    val_data, val_lengths, val_labels = getData(spectrograms,cats,lengths, test=True)
+    test(val_data, val_lengths, val_labels)
 
 run()
